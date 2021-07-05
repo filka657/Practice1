27bf4bcd3bad4aa8540bf887732a85dbd97ae21c
filka657 <72990504+filka657@users.noreply.github.com>
2021-07-05 21:59:52 +0300

    Add files via upload

---
+++ Задания 3 и 4 со списком.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int val;
+	Node* next;
+};
+
+void init(Node* node)
+{
+	node->val = 0;
+	node->next = NULL;
+}
+
+void pushNode(Node* root, Node* node)
+{
+	Node* cur = root;
+	while (cur->next != NULL)
+		cur = cur->next;
+
+	cur->next = new Node;
+	init(cur->next);
+	cur->val = node->val;
+}
+
+void lShiftList(Node* head, int n) {
+	for (int i = 0; i < n; i++)
+	{
+		Node* elem = head;
+		int num = elem->val;
+
+		while (elem->next->next != NULL)
+		{
+			elem->val = elem->next->val;
+			elem = elem->next;
+		}
+
+		elem->val = num;
+	}
+}
+
+bool testPushNode()
+{
+	Node* head = new Node;
+
+	init(head);
+	pushNode(head, new Node{ 1 });
+	pushNode(head, new Node{ 2 });
+	return (head->next->val == 2 && head->val == 1);
+}
+
+bool testInitNode()
+{
+	Node* root = new Node;
+
+	init(root);
+	return (root->val == 0);
+
+}
+
+bool testlShiftList() {
+	Node* head = new Node;
+
+	init(head);
+	pushNode(head, new Node{ 1 });
+	pushNode(head, new Node{ 2 });
+	pushNode(head, new Node{ 3 });
+	pushNode(head, new Node{ 4 });
+	lShiftList(head, 3);
+	return (head->val == 4 && head->next->val == 1 && head->next->next->val == 2 && head->next->next->next->val == 3);
+}
+
+static void runTest(bool (*testFunction)(), const string& testName)
+{
+	if (testFunction() == 1)
+		std::cout << "Test " << testName << " - OK" << std::endl;
+	else
+		std::cout << "Test " << testName << " - FAIL" << std::endl;
+}
+
+int main()
+{
+	runTest(testInitNode, "Init test");
+	runTest(testPushNode, "Push test");
+}
